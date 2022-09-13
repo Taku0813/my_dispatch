95817321f4558aee7baa0223aa884659e4e62df5
Taku0813 <taku08132001@icloud.com>
2022-09-13 15:46:27 +0900

    firebase setting

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,25 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
-void main() {
   runApp(const MyApp());
 }
 
+// void main() {
+//   runApp(const MyApp());
+// }
+//
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext cont9ext) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
@@ -116,3 +124,4 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+
